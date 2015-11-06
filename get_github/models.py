58c75f943a509700047fd4f824868b59b0ce67b9
wumin <wuminorb@gmail.com>
2015-11-06 16:23:51 +0800

    fix syntax error o(╯□╰)o

--- get_github/models.py
+++ get_github/models.py
@@ -2,24 +2,24 @@ from django.db import models
 
 
 class GithubUser(models.Model):
-    blog = models.URLField
-    company = models.TextField
-    create_at = models.DateTimeField
-    emile = models.EmailField
+    blog = models.URLField(null=True)
+    company = models.CharField(null=True, max_length=255)
+    create_at = models.DateTimeField()
+    emile = models.EmailField(null=True)
     following = models.ManyToManyField("self", symmetrical=False)
-    hireable = models.NullBooleanField
-    location = models.TextField
-    login = models.TextField
-    name = models.TextField
-    public_repo_num = models.IntegerField
-    update_at = models.DateTimeField
+    hireable = models.NullBooleanField()
+    location = models.CharField(null=True, max_length=255)
+    login = models.CharField(max_length=255)
+    name = models.CharField(null=True, max_length=255)
+    public_repo_num = models.IntegerField(default=0)
+    update_at = models.DateTimeField()
 
 
 class GithubRepo(models.Model):
-    full_name = models.TextField
-    description = models.TextField
-    stargazers_count = models.IntegerField
-    watchers_count = models.IntegerField
-    forks_count = models.IntegerField
-    create_at = models.DateTimeField
-    pushed_at = models.DateTimeField
+    full_name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    stargazers_count = models.IntegerField(default=0)
+    watchers_count = models.IntegerField(default=0)
+    forks_count = models.IntegerField(default=0)
+    create_at = models.DateTimeField()
+    pushed_at = models.DateTimeField()
