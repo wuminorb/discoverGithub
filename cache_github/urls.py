36a035637421977a2f1fb389c1277aa824db88e3
wumin <wuminorb@gmail.com>
2015-11-11 12:08:17 +0800

    fix username regular expression

--- cache_github/urls.py
+++ cache_github/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # ex: /github/user/wuminorb
-    url(r'^user/(?P<pk>[a-zA-Z0-9]+)/$', views.UserDetailView.as_view(), name='user_detail'),
-    url(r'^refresh/user/(?P<login>[a-zA-Z0-9]+)/$', views.refresh_user, name='refresh_user'),
-    url(r'^refresh/user_following/(?P<login>[a-zA-Z0-9]+)/$', views.refresh_following, name='refresh_user_following'),
+    url(r'^user/(?P<pk>[a-zA-Z0-9_-]+)/$', views.UserDetailView.as_view(), name='user_detail'),
+    url(r'^refresh/user/(?P<login>[a-zA-Z0-9_-]+)/$', views.refresh_user, name='refresh_user'),
+    url(r'^refresh/user_following/(?P<login>[a-zA-Z0-9_-]+)/$', views.refresh_following, name='refresh_user_following'),
 ]
